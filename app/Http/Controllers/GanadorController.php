b2955fd90dfcae981d29d55653b3bd2b04b83419
nicola-19010 <101908582+nicola-19010@users.noreply.github.com>
2023-11-12 20:57:40 -0300

    commit 2

--- app/Http/Controllers/GanadorController.php
+++ app/Http/Controllers/GanadorController.php
@@ -12,7 +12,7 @@ class GanadorController extends Controller
     public function registrarGanador(Request $request){
         
         $ganador = new Ganador();
-        $ganador->nombre = $request->nombre;
+        $ganador->nombre = $request->nombre; 
         $ganador->save(); 
 
         return response()->json(['mensaje' => 'Se ha registrado con éxito']);
